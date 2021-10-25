9f0661bb9e6de1e37e57fd5aef08c118dfdd43b4
Piero <piero071198@gmail.com>
2021-10-25 13:59:55 +0200

    Primer Comit

---
+++ src/main/java/com/everis/EverisGit1PEVR/App.java
@@ -0,0 +1,13 @@
+package com.everis.EverisGit1PEVR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
